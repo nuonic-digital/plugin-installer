7782d89235214ef894bc61f4f7db2b37a3d1b08b
Alex Jank <alex@nuonic.de>
2025-03-07 23:08:45 +0100

    chore: refactor some statements

--- src/Action/LoadPluginAction.php
+++ src/Action/LoadPluginAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace NuonicPluginInstaller\Action;
 
 use Composer\Semver\Semver;
+use NuonicPluginInstaller\Core\Framework\Plugin\AvailableOpensourcePlugin\AvailableOpensourcePluginEntity;
 use NuonicPluginInstaller\Service\IndexFileServiceInterface;
 use NuonicPluginInstaller\Struct\PackageIndexEntry;
 use Shopware\Core\Framework\Adapter\Cache\CacheValueCompressor;
@@ -16,185 +17,190 @@ use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 readonly class LoadPluginAction
 {
-	public function __construct(
-		private readonly EntityRepository $availableOpensourcePluginRepository,
-		private readonly IndexFileServiceInterface $indexFileService,
-		private HttpClientInterface $httpClient,
-		private readonly CacheInterface $cache,
-		private readonly EntityRepository $languageRepository,
-		private readonly string $shopwareVersion,
-	) {
-		// This is a constructor
-	}
+    public function __construct(
+        private EntityRepository $availableOpensourcePluginRepository,
+        private IndexFileServiceInterface $indexFileService,
+        private HttpClientInterface $httpClient,
+        private CacheInterface $cache,
+        private EntityRepository $languageRepository,
+        private string $shopwareVersion,
+    ) {
+    }
 
-	public function execute(string|PackageIndexEntry $packageInformation): void
-	{
-		if (!$packageInformation instanceof PackageIndexEntry) {
-			$packageInformation = $this->indexFileService->getPackageInformation($packageInformation);
-		}
+    public function execute(string|PackageIndexEntry $packageInformation): void
+    {
+        if (!$packageInformation instanceof PackageIndexEntry) {
+            $packageInformation = $this->indexFileService->getPackageInformation($packageInformation);
+        }
 
-		if (null === $packageInformation) {
-			return;
-		}
+        if (null === $packageInformation) {
+            return;
+        }
 
-		$repositoryUrl = $packageInformation->repositoryUrl;
-		$ref = $packageInformation->ref;
+        $repositoryUrl = $packageInformation->repositoryUrl;
+        $ref = $packageInformation->ref;
 
-		$packagistData = $this->getPackagistData($ref);
+        $packagistData = $this->getPackagistData($ref);
 
-		$version = $this->findSuitableVersion($packagistData);
+        $version = $this->findSuitableVersion($packagistData);
 
-		if ($version === null) {
-			return;
-		}
+        if (null === $version) {
+            return;
+        }
 
-		$githubUrl = \str_replace('.git', '', $version['source']['url']);
-		$githubUrl = \str_replace('https://github.com/', 'https://raw.githubusercontent.com/', $githubUrl . '/refs/heads/main/');
-		$mainExtensionYmlUrl = $githubUrl . '.shopware-extension.yml';
+        $githubUrl = \str_replace('.git', '', $version['source']['url']);
+        $githubUrl = \str_replace('https://github.com/', 'https://raw.githubusercontent.com/', $githubUrl.'/refs/heads/main/');
+        $mainExtensionYmlUrl = $githubUrl.'.shopware-extension.yml';
 
-		$extensionYmlUrl = null;
+        $extensionYmlUrl = null;
 
-		$isComposer = false;
+        $isComposer = false;
 
-		$mainExtensionYmlResponse = $this->httpClient->request('GET', $mainExtensionYmlUrl);
-		if ($mainExtensionYmlResponse->getStatusCode() === 200) {
-			$extensionYmlUrl = $mainExtensionYmlUrl;
-		} else {
-			$isComposer = true;
-		}
+        $mainExtensionYmlResponse = $this->httpClient->request('GET', $mainExtensionYmlUrl);
+        if (200 === $mainExtensionYmlResponse->getStatusCode()) {
+            $extensionYmlUrl = $mainExtensionYmlUrl;
+        } else {
+            $isComposer = true;
+        }
 
-		$pluginData = [
-			'packageName' => $packageInformation->packageName,
-			'manufacturer' => \implode(', ', \array_column($version['authors'], 'name')),
-			'manufacturerLink' => $version['extra']['manufacturerLink']['en-GB'],
-			'license' => $version['license'][0],
-			'link' => $repositoryUrl,
-			'availableVersion' => $version['version']
-		];
+        $pluginData = [
+            'packageName' => $packageInformation->packageName,
+            'manufacturer' => \implode(', ', \array_column($version['authors'], 'name')),
+            'manufacturerLink' => $version['extra']['manufacturerLink']['en-GB'],
+            'license' => $version['license'][0],
+            'link' => $repositoryUrl,
+            'availableVersion' => $version['version'],
+        ];
 
+        if (!$isComposer) {
+            $extensionYmlResponse = $this->httpClient->request('GET', $extensionYmlUrl);
+            if (200 === $extensionYmlResponse->getStatusCode()) {
+                $extensionYmlData = Yaml::parse($extensionYmlResponse->getContent());
+                if (isset($extensionYmlData['store'])) {
+                    if (isset($extensionYmlData['store']['icon'])) {
+                        $pluginData['icon'] = $githubUrl.'/'.$extensionYmlData['store']['icon'];
+                    } else {
+                        $pluginData['icon'] = $githubUrl.'/src/Resources/config/plugin.png';
+                    }
+                    foreach ($extensionYmlData['store']['images'] as $images) {
+                        $pluginData['images'][] = $githubUrl.'/'.$images['file'];
+                    }
+                    if (isset($extensionYmlData['store']['description']['en'])) {
+                        $pluginData['description']['en-GB'] = $extensionYmlData['store']['description']['en'];
+                    }
+                    if (isset($extensionYmlData['store']['description']['de'])) {
+                        $pluginData['description']['de-DE'] = $extensionYmlData['store']['description']['de'];
+                    }
 
-		if (!$isComposer) {
-			$extensionYmlResponse = $this->httpClient->request('GET', $extensionYmlUrl);
-			if ($extensionYmlResponse->getStatusCode() === 200) {
-				$extensionYmlData = Yaml::parse($extensionYmlResponse->getContent());
-				if (isset($extensionYmlData['store'])) {
-					if (isset($extensionYmlData['store']['icon'])) {
-						$pluginData['icon'] = $githubUrl . '/' . $extensionYmlData['store']['icon'];
-					} else {
-						$pluginData['icon'] = $githubUrl . '/src/Resources/config/plugin.png';
-					}
-					foreach ($extensionYmlData['store']['images'] as $images) {
-						$pluginData['images'][] = $githubUrl . '/' . $images['file'];
-					}
-					if (isset($extensionYmlData['store']['description']['en'])) {
-						$pluginData['description']['en-GB'] = $extensionYmlData['store']['description']['en'];
-					}
-					if (isset($extensionYmlData['store']['description']['de'])) {
-						$pluginData['description']['de-DE'] = $extensionYmlData['store']['description']['de'];
-					}
+                // TODO Load from markdown files
 
-					// TODO Load from markdown files
+                // TODO load images from folder
+                } else {
+                    $pluginData['icon'] = $githubUrl.'/src/Resources/config/plugin.png';
+                    $isComposer = true;
+                }
+            }
+        }
 
-					// TODO load images from folder
+        if ($isComposer) {
+            $pluginData['description']['en-GB'] = $version['extra']['description']['en-GB'] ?? $packageInformation->packageName;
+            $pluginData['description']['de-DE'] = $version['extra']['description']['de-DE'] ?? $packageInformation->packageName;
+        }
 
-				} else {
-					$pluginData['icon'] = $githubUrl . '/src/Resources/config/plugin.png';
-					$isComposer = true;
-				}
-			}
-		}
+        $pluginData['name']['de-DE'] = $version['extra']['label']['de-DE'] ?? $packageInformation->packageName;
+        $pluginData['name']['en-GB'] = $version['extra']['label']['en-GB'] ?? $packageInformation->packageName;
 
-		if ($isComposer) {
-			$pluginData['description']['en-GB'] = isset($version['extra']['description']['en-GB']) ? $version['extra']['description']['en-GB'] : $packageInformation->packageName;
-			$pluginData['description']['de-DE'] = isset($version['extra']['description']['de-DE']) ? $version['extra']['description']['de-DE'] : $packageInformation->packageName;
-		}
+        $langIdDe = $this->loadLanguageId('de-DE');
+        $langIdEn = $this->loadLanguageId('en-GB');
 
-		$pluginData['name']['de-DE'] = isset($version['extra']['label']['de-DE']) ? $version['extra']['label']['de-DE'] : $packageInformation->packageName;
-		$pluginData['name']['en-GB'] = isset($version['extra']['label']['en-GB']) ? $version['extra']['label']['en-GB'] : $packageInformation->packageName;
+        $pluginData['translations'] = [
+            ['languageId' => $langIdDe, 'name' => $pluginData['name']['de-DE'], 'description' => $pluginData['description']['de-DE']],
+            ['languageId' => $langIdEn, 'name' => $pluginData['name']['en-GB'], 'description' => $pluginData['description']['en-GB']],
+        ];
 
-		$langIdDe = $this->loadLanguageId('de-DE');
-		$langIdEn = $this->loadLanguageId('en-GB');
+        unset($pluginData['description'], $pluginData['name']);
 
-		$pluginData['translations'] = [
-			['languageId' => $langIdDe, 'name' => $pluginData['name']['de-DE'], 'description' => $pluginData['description']['de-DE']],
-			['languageId' => $langIdEn, 'name' => $pluginData['name']['en-GB'], 'description' => $pluginData['description']['en-GB']],
-		];
+        /** @var AvailableOpensourcePluginEntity|null $plugin */
+        $plugin = $this->availableOpensourcePluginRepository->search(
+            (new Criteria())->addFilter(new EqualsFilter('packageName', $packageInformation->packageName)),
+            Context::createDefaultContext()
+        )->first();
 
-		unset($pluginData['description']);
-		unset($pluginData['name']);
+        $id = $plugin?->getId() ?? Uuid::randomHex();
 
-		/** @var \NuonicPluginInstaller\Core\Framework\Plugin\AvailableOpensourcePlugin\AvailableOpensourcePluginEntity|null */
-		$plugin = $this->availableOpensourcePluginRepository->search(
-			(new Criteria())->addFilter(new EqualsFilter('packageName', $packageInformation->packageName)),
-			Context::createDefaultContext()
-		)->first();
+        $pluginData['id'] = $id;
+        $pluginData['isInstalled'] = $plugin && $plugin->isInstalled();
 
-		$id = $plugin?->getId() ?? Uuid::randomHex();
+        $this->availableOpensourcePluginRepository->upsert([$pluginData], Context::createDefaultContext());
+    }
 
-		$pluginData['id'] = $id;
-		$pluginData['isInstalled'] = $plugin ? $plugin->isInstalled() : false;
+    private function findSuitableVersion(array $packagistData): ?array
+    {
+        $versions = $packagistData['package']['versions'];
+        \ksort($versions, SORT_NATURAL);
 
-		$this->availableOpensourcePluginRepository->upsert([$pluginData], Context::createDefaultContext());
-	}
+        $versions = \array_reverse($versions, true);
 
+        foreach ($versions as $version => $versionData) {
+            if (\str_contains($version, 'dev') || \str_contains($version, 'alpha') || \str_contains($version, 'beta') || \str_contains($version, 'rc') || \str_contains($version, 'main')) {
+                continue;
+            }
 
-	private function findSuitableVersion(array $packagistData): ?array
-	{
-		$versions = $packagistData['package']['versions'];
-		\ksort($versions, SORT_NATURAL);
+            if ($this->checkVersionConstraint($versionData['require']['shopware/core'], $this->shopwareVersion)) {
+                return $versionData;
+            }
+        }
 
-		$versions = \array_reverse($versions, true);
+        return null;
+    }
 
-		foreach ($versions as $version => $versionData) {
-			if (\str_contains($version, 'dev') || \str_contains($version, 'alpha') || \str_contains($version, 'beta') || \str_contains($version, 'rc') || \str_contains($version, 'main')) {
-				continue;
-			}
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    private function getPackagistData(string $ref)
+    {
+        $response = $this->httpClient->request('GET', $ref);
 
-			if ($this->checkVersionConstraint($versionData['require']['shopware/core'], $this->shopwareVersion)) {
-				return $versionData;
-			}
-		}
+        if (200 !== $response->getStatusCode()) {
+            return null;
+        }
 
-		return null;
-	}
+        $packagistData = json_decode($response->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
-	private function getPackagistData(string $ref)
-	{
-		$response = $this->httpClient->request('GET', $ref);
+        return $packagistData;
+    }
 
-		if ($response->getStatusCode() !== 200) {
-			return null;
-		}
+    private function checkVersionConstraint(string $constraint, string $version): bool
+    {
+        return Semver::satisfies($version, $constraint);
+    }
 
-		$packagistData = json_decode($response->getContent(), true);
-		return $packagistData;
-	}
+    private function loadLanguageId(string $locale): string
+    {
+        $key = 'nuonic-plugin-locale-'.$locale;
+        $context = Context::createDefaultContext();
 
-	private function checkVersionConstraint(string $constraint, string $version): bool
-	{
-		return Semver::satisfies($version, $constraint);
-	}
+        $value = $this->cache->get($key, function (ItemInterface $item) use ($locale, $context) {
+            $languageCriteria = new Criteria();
+            $languageCriteria->addFilter(new EqualsFilter('locale.code', $locale));
+            $languageCriteria->setLimit(1);
 
-	private function loadLanguageId(string $locale)
-	{
-		$key = 'nuonic-plugin-locale-' . $locale;
-		$context = Context::createDefaultContext();
+            $languageId = $this->languageRepository->searchIds($languageCriteria, $context)->firstId();
 
-		$value = $this->cache->get($key, function (ItemInterface $item) use ($locale, $context) {
+            return CacheValueCompressor::compress($languageId);
+        });
 
-			$languageCriteria = new Criteria();
-			$languageCriteria->addFilter(new EqualsFilter('locale.code', $locale));
-			$languageCriteria->setLimit(1);
-
-			$languageId = $this->languageRepository->searchIds($languageCriteria, $context)->firstId();
-
-			return CacheValueCompressor::compress($languageId);
-		});
-
-		return CacheValueCompressor::uncompress($value);
-	}
+        return CacheValueCompressor::uncompress($value);
+    }
 }
