d6175dd2fdd71c0a909954a2f30a29390f13f777
Alex Jank <alex@nuonic.de>
2025-03-07 20:55:46 +0100

    feat(index): load index after update / install

--- src/NuonicPluginInstaller.php
+++ src/NuonicPluginInstaller.php
@@ -5,20 +5,37 @@ declare(strict_types=1);
 namespace NuonicPluginInstaller;
 
 use Doctrine\DBAL\Connection;
+use NuonicPluginInstaller\Action\LoadIndexAction;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 
 class NuonicPluginInstaller extends Plugin
 {
-	public function uninstall(UninstallContext $context): void
-	{
-		parent::uninstall($context);
-		if ($context->keepUserData()) {
-			return;
-		}
-		$connection = $this->container->get(Connection::class);
+    public function uninstall(UninstallContext $context): void
+    {
+        parent::uninstall($context);
+        if ($context->keepUserData()) {
+            return;
+        }
+        $connection = $this->container->get(Connection::class);
 
-		$connection->executeStatement('DROP TABLE IF EXISTS nuonic_available_opensource_plugin_translation');
-		$connection->executeStatement('DROP TABLE IF EXISTS nuonic_available_opensource_plugin');
-	}
+        $connection->executeStatement('DROP TABLE IF EXISTS nuonic_available_opensource_plugin_translation');
+        $connection->executeStatement('DROP TABLE IF EXISTS nuonic_available_opensource_plugin');
+    }
+
+    public function postInstall(InstallContext $installContext): void
+    {
+        if ($action = $this->container->get(LoadIndexAction::class)) {
+            $action->execute();
+        }
+    }
+
+    public function postUpdate(UpdateContext $updateContext): void
+    {
+        if ($action = $this->container->get(LoadIndexAction::class)) {
+            $action->execute();
+        }
+    }
 }
